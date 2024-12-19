00c371593cff5a5564c84dc96f96ff4c853c0014
HCA31 <hochian31@gmail.com>
2024-12-19 15:00:34 +0700

    create button next&prev carousel

---
+++ src/app/components/SampleNextArrow.tsx
@@ -0,0 +1,20 @@
+import { ArrowLeft } from './icon';
+
+interface SampleNextArrowProps {
+	className?: string;
+	onClick?: () => void;
+}
+
+const SampleNextArrow = (props: SampleNextArrowProps) => {
+	const { className, onClick } = props;
+	return (
+		<div
+			className={`absolute -right-12 top-1/2 z-[100] flex h-[36px] w-[36px] cursor-pointer items-center justify-center rounded-full bg-white ${className}`}
+			onClick={onClick}
+		>
+			<ArrowLeft className='rotate-180' />
+		</div>
+	);
+};
+
+export default SampleNextArrow;
---
+++ src/app/components/icon.tsx
@@ -0,0 +1,18 @@
+export const ArrowLeft = ({ className = '', width = '22px', height = '16px' }: any) => (
+	<svg
+		width={width}
+		height={height}
+		viewBox='0 0 14 22'
+		fill='none'
+		xmlns='http://www.w3.org/2000/svg'
+		className={className}
+	>
+		<path
+			d='M11.1501 2.17499L2.3501 11L11.1501 19.825'
+			stroke='#F0284A'
+			stroke-width='4'
+			stroke-linecap='round'
+			stroke-linejoin='round'
+		/>
+	</svg>
+);
